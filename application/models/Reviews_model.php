6d0feab8983996fc100d4cef6741ae9c45cb2cf2
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-05-04 00:47:25 +0200

    -Reviews

--- application/models/Reviews_model.php
+++ application/models/Reviews_model.php
@@ -27,16 +27,22 @@ class Reviews_model extends CI_Model {
 	public function get_shop_name($id, $type) {
 
 		if ($id != 0 && $id != '' && $type != "") {
-			$this->db->select('name');
 			$this->db->where('id', $id);
 			if ($type == "workshop") {
+				$this->db->select('name');
 				$this->db->from('workshop');
 			}
 			if ($type == "partsshop") {
+				$this->db->select('name');
 				$this->db->from('partshop');
 			}
 			if ($type == "serviceshop") {
+				$this->db->select('name');
 				$this->db->from('service_shop');
+			}
+			if ($type == "provider") {
+				$this->db->select('user_name as name');
+				$this->db->from('provider_user');
 			}
 			$q = $this->db->get();
 			if ($q->num_rows() > 0) {
