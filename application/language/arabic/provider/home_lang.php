9f58834d557007c5d18b6128e4d4e8b101dfdc05
NAHRDEV\mohamed.hamdy <mohamed.hamdy@nahrdev.com>
2020-05-11 00:09:08 +0200

    lang

--- application/language/arabic/provider/home_lang.php
+++ application/language/arabic/provider/home_lang.php
@@ -1,11 +1,4 @@
 <?php
-$lang["Dashboard"] = "الرئيسية";
-$lang["Parts"] = "القطع";
-$lang["Profile"] = "الصفحة الشخصية";
-$lang["Plans"] = "الخطة";
-$lang["Plan_history"] = "سجل الخطط";
-$lang["Shipping"] = "التوصيل";
-$lang["Shipping_request"] = "طلبات التوصيل";
 
 $lang["Current_Plan_Title"] = "المخطط_الحالي";
 $lang["Current_Plan_Duration"] = "التخطيط الحالي";
