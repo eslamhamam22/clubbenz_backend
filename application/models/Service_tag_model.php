22290a7514b0e4ea31d363f9d6ae7dab0b30c6ea
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-06-11 14:46:59 +0200

    Search fixed

--- application/models/Service_tag_model.php
+++ application/models/Service_tag_model.php
@@ -11,26 +11,29 @@ class Service_tag_model extends CI_Model{
 	}
 	
 	function distance($lat1, $lon1, $lat2, $lon2, $unit) {
-		  if (($lat1 == $lat2) && ($lon1 == $lon2)) {
-		    return 0;
-		  }
-		  else {
-		    $theta = $lon1 - $lon2;
-		    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-		    $dist = acos($dist);
-		    $dist = rad2deg($dist);
-		    $miles = $dist * 60 * 1.1515;
-		    $unit = strtoupper($unit);
+		if($lat1 && $lon1 && $lat2 && $lon2 && is_numeric($lat1) && is_numeric($lon1) && is_numeric($lat2) && is_numeric($lon2)){
+			if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+				return 0;
+			}
+			else {
+				$theta = $lon1 - $lon2;
+				$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+				$dist = acos($dist);
+				$dist = rad2deg($dist);
+				$miles = $dist * 60 * 1.1515;
+				$unit = strtoupper($unit);
 
-		    if ($unit == "K") {
-		      return ($miles * 1.609344);
-		    } else if ($unit == "N") {
-		      return ($miles * 0.8684);
-		    } else {
-		      return $miles;
-		    }
-		  }
+				if ($unit == "K") {
+					return ($miles * 1.609344);
+				} else if ($unit == "N") {
+					return ($miles * 0.8684);
+				} else {
+					return $miles;
+				}
+			}
+
 		}
+	}
 		
 	
 	public function manage_serviceshop(){
